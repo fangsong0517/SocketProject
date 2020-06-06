a51330f62348180ab69fb8d4e061449ebd5b888d
fangsong0517 <2035756541@qq.com>
2020-06-06 23:03:13 +0800

    tcp_damon

---
+++ tcp_damon/test_o_non_block.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: test_o_non_block.c
+	> Author: suyelu
+	> Mail: suyelu@haizeix.com
+	> Created Time: 日  3/29 14:13:10 2020
+ ************************************************************************/
+
+#include "../common/head.h"
+
+int main () {
+    printf("%d\n",O_CREAT);
+}
+
